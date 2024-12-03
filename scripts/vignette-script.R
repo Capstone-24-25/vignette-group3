877fb73ce899266de9fc4f01edc72eaa73bed9bf
tessivinjack <ktivinjack@ucsb.edu>
2024-12-02 18:27:28 -0800

    commented script updated

--- scripts/vignette-script.R
+++ scripts/vignette-script.R
@@ -1,23 +1,6 @@
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
 
-# Primary Vignette - Classification Strategies
+# Commented Vignette Script
 
-**Objectives**
-  
-  -   Perform exploratory data analysis
-
--   Reduce dimensionality using principal component analysis
-
--   Employ logistic regression using `glm()` and `multinom()`
-
--   
-  
-  We'll illustrate these strategies using the `California Household Travel Survey (CHTS)` dataset.
-
-```{r}
-# Loading necessary packages
 library(tidyverse)
 library(ggplot2)
 library(dplyr)
@@ -55,9 +38,6 @@ scaled_data <- cbind(hhid, bg_group, scaled_data)
 scaled_data_clean <- na.omit(scaled_data) %>% 
   as.data.frame()
 
-```
-
-```{r}
 #partition data
 set.seed(14531)
 partitions <- scaled_data_clean %>% 
@@ -73,10 +53,7 @@ train_dtm <- training(partitions) %>%
   select(-hhid, -bg_group)
 train_labels <- training(partitions) %>%
   select(hhid, bg_group)
-```
 
-```{r}
-set.seed(14531)
 
 #Convert training data to sparse matrix to use sparsesvd function to perform PCA
 train_dtm_sparse <- as.matrix(train_dtm) %>% 
@@ -88,10 +65,6 @@ training_projected <- as.data.frame(train_svd$u %*% diag(train_svd$d))
 
 #assign column names
 colnames(training_projected) <- paste0("PC", 1:ncol(training_projected))
-```
-
-```{r}
-set.seed(14531)
 
 #function to reproject new data onto training PCA
 reproject_fn <- function(.dtm, train_projected) {
@@ -103,9 +76,7 @@ reproject_fn <- function(.dtm, train_projected) {
 
 #project test data onto training PCA
 test_projected <- reproject_fn(test_dtm, train_svd)
-```
 
-```{r}
 #explained variance plot
 singular_values <- train_svd$d
 variance_explained <- (singular_values^2) / sum(singular_values^2)
@@ -119,9 +90,7 @@ cumulative_variance <- cumsum(variance_explained)
 
 plot(cumulative_variance, type = "b", xlab = "Principal Component",
      ylab = "Cumulative Variance Explained", main = "Cumulative Variance")
-```
 
-```{r}
 # set the threshold for the cumulative variance (80%)
 threshold <- 0.8
 reduced_pcs <- which(cumulative_variance >= threshold)[1]
@@ -129,15 +98,11 @@ reduced_pcs <- which(cumulative_variance >= threshold)[1]
 # print the number of PCs to keep
 cat("Number of PCs to retain:", reduced_pcs)
 
-```
-
-```{r}
 # Plot PC1 vs PC2
 plot(training_projected$PC1, training_projected$PC2, xlab = "PC1", ylab = "PC2",
      main = "PCA - PC1 vs PC2", pch = 19, col = "blue")
-```
 
-```{r}
+
 # Add the categorical variable to the PC scores
 plot_pca_train <- cbind(training_projected, train_labels)  # Replace with actual variable
 
@@ -150,39 +115,24 @@ ggplot(plot_pca_train, aes(x = PC1, y = PC2, color = as.factor(bg_group))) +
   theme_minimal()+
   scale_color_manual(values = c('green', 'blue', 'red', 'yellow'))
 
-```
-
 ## Logistic Regression
 
-Get the reduced PCA data that you will feed into logistic regression model
-
-```{r}
+# Get the reduced PCA data that you will feed into logistic regression model
 training_projected_reduced <- training_projected[, 1:reduced_pcs]
 test_projected_reduced <- test_projected[, 1:reduced_pcs]
 
 reduced_training <- cbind(training_projected_reduced, bg_group = train_labels$bg_group)
 
 reduced_testing <- cbind(as.data.frame(test_projected_reduced), bg_group = test_labels$bg_group)
-```
 
-Fit logistic regression model with the PCA reduced training data
+# Fit logistic regression model with the PCA reduced training data
 
-```{r}
+
 library(nnet)
 log_regmodel <- multinom(bg_group ~ ., data = reduced_training)
-```
 
-Let's see how well our model was able to classify the different household densities
+# Let's see how well our model was able to classify the different household densities
 
-```{r}
 logreg_test_predictions <- predict(multinom_model, newdata = reduced_testing)
 
-```
-
 ## Saving data
-
-```{r}
-save(training_projected, file ="../Data/training_projected.RData")
-save(test_projected, file = "../Data/test_projected.RData")
-
-```
